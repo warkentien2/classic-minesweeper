ef3564bf5998ab3b0caceab0e2fb485616340f69
Philip Warkentien II <philip.dw2@gmail.com>
2023-03-23 23:32:26 -0500

    Remove the Input component

--- src/MinesweeperUI/index.ts
+++ src/MinesweeperUI/index.ts
@@ -1,7 +1,6 @@
 export { Board } from "./Board/Board";
 export { Counter } from "./Counter/Counter";
 export { Digit } from "./Digit/Digit";
-export { Input } from "./Input/Input";
 export { Link } from "./Link/Link";
 export { LinkGroup } from "./LinkGroup/LinkGroup";
 export { Modal } from "./Modal/Modal";
